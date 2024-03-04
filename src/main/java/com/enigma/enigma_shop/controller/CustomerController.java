a8070f42d96469447b79509456d924a05e17af69
Rifqi Ramadhan <rifqyomp@gmail.com>
2024-03-04 20:29:16 +0700

    improve: change response and request and constant message update :art:

--- src/main/java/com/enigma/enigma_shop/controller/CustomerController.java
+++ src/main/java/com/enigma/enigma_shop/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.enigma.enigma_shop.controller;
 
 import com.enigma.enigma_shop.constant.APIUrl;
+import com.enigma.enigma_shop.constant.ResponseMessage;
 import com.enigma.enigma_shop.dto.request.SearchCustomerRequest;
 import com.enigma.enigma_shop.dto.request.UpdateCustomerRequest;
+import com.enigma.enigma_shop.dto.response.CommonResponse;
 import com.enigma.enigma_shop.dto.response.CustomerResponse;
 import com.enigma.enigma_shop.entity.Customer;
 import com.enigma.enigma_shop.service.CustomerService;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,17 +25,21 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @GetMapping(path = "/{id}",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable String id) {
+    public ResponseEntity<CommonResponse<CustomerResponse>> getCustomerById(@PathVariable String id) {
         CustomerResponse customer = customerService.getOneById(id);
-        return ResponseEntity.ok(customer);
+        CommonResponse<CustomerResponse> response = CommonResponse.<CustomerResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessage.SUCCESS_GET_DATA)
+                .data(customer)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'ADMIN')")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<CustomerResponse>> getAllCustomer(
+    public ResponseEntity<CommonResponse<List<CustomerResponse>>> getAllCustomer(
             @RequestParam(name = "name", required = false) String name,
             @RequestParam(name = "mobilePhoneNo", required = false) String phoneNumber,
             @RequestParam(name = "birthDate", required = false) @JsonFormat(pattern = "yyyy-MM-dd") String birthDate,
@@ -46,30 +53,50 @@ public class CustomerController {
                 .build();
 
         List<CustomerResponse> customers = customerService.getAll(request);
-        return ResponseEntity.ok(customers);
+
+        CommonResponse<List<CustomerResponse>> response = CommonResponse.<List<CustomerResponse>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessage.SUCCESS_GET_DATA)
+                .data(customers)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<CustomerResponse> updateCustomer(@RequestBody UpdateCustomerRequest payload) {
+    public ResponseEntity<CommonResponse<CustomerResponse>> updateCustomer(@RequestBody UpdateCustomerRequest payload) {
         CustomerResponse customer = customerService.update(payload);
-        return ResponseEntity.ok(customer);
+        CommonResponse<CustomerResponse> response = CommonResponse.<CustomerResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessage.SUCCESS_UPDATE_DATA)
+                .data(customer)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> updateStatusCustomer(
+    public ResponseEntity<CommonResponse<String>> updateStatusCustomer(
             @PathVariable(name = "id") String id,
             @RequestParam(name = "status") Boolean status
     ) {
         customerService.updateStatusById(id, status);
-        return ResponseEntity.ok("OK");
+        CommonResponse<String> response = CommonResponse.<String>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessage.SUCCESS_UPDATE_DATA)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> deleteById(@PathVariable String id) {
+    public ResponseEntity<CommonResponse<String>> deleteById(@PathVariable String id) {
         customerService.deleteById(id);
-        return ResponseEntity.ok("OK");
+        CommonResponse<String> response = CommonResponse.<String>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(ResponseMessage.SUCCESS_DELETE_DATA)
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
